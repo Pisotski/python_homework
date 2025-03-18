cdfa5a1ecf4c0f6e491f7438985c0fb6e4d48c90
Pisotski <vlad.pisotski@gmail.com>
2025-03-17 17:56:21 -0700

    a1 homework done

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -95,13 +95,70 @@ def titleize(string):
     # (1) The first word is always capitalized.
     # (2) The last word is always capitalized.
     # (3) All the other words are capitalized, except little words.
-
+    little_words = ["a", "on", "an", "the", "of", "and", "is", "in"]
     # For the purposes of this task, the little words are "a", "on", "an", "the", "of", "and", "is", and "in".
     # split(' '), join(), and capitalize().
     # words[-1] gives the last element in the list.
-
     # The in comparison operator: You have seen in used in loops. But it can also be used for comparisons,
     # for example to check to see if a substring occurs in a string, or a value occurs in a list.
     # A new trick: As you loop through the words in the words list,
     # it is helpful to have the index of the word for each iteration. You can access that index using the enumerate() function:
-    return
+    words_list = string.split()
+    last_word_index = len(words_list) - 1
+    for index, word in enumerate(words_list):
+        if index == 0 or index == last_word_index:
+            word = word.capitalize()
+            words_list[index] = word
+        elif word in little_words:
+            continue
+        else:
+            word = word.capitalize()
+            words_list[index] = word
+    return " ".join(words_list)
+
+
+def hangman(secret, guess):
+    result = ""
+    guess_list = list(guess)
+    for letter in secret:
+        if letter in guess_list:
+            result += letter
+        else:
+            result += "_"
+    return result
+
+
+# Pig Latin is a kid's trick language.
+# Each word is modified according to the following rules.
+# (1) If the string starts with a vowel (aeiou), "ay" is tacked onto the end.
+# (2) If the string starts with one or several consonants, they are moved to the end and "ay" is tacked on after them.
+# (3) "qu" is a special case, as both of them get moved to the end of the word, as if they were one consonant letter.
+# Create a function called pig_latin. It takes an English string or sentence and converts it to Pig Latin, returning the result.
+# We will assume that there is no punctuation and that everything is lower case.
+
+
+def pig_latin(sequence):
+    vowels = "aeiou"
+    vowels_list = [*vowels]
+    words_list = sequence.split(" ")
+
+    for key, word in enumerate(words_list):
+        if word[0] in vowels_list:
+            words_list[key] = word + "ay"
+        else:
+            pointer = 0
+            tail = ""
+            while pointer < len(word):
+                letter = word[pointer]
+                if letter in vowels_list:
+                    break
+                if (letter + word[pointer + 1]) == "qu":
+                    tail += "qu"
+                    pointer += 2
+                    break
+                else:
+                    tail += letter
+                    pointer += 1
+            words_list[key] = word[pointer:] + tail + "ay"
+
+    return " ".join(words_list)
